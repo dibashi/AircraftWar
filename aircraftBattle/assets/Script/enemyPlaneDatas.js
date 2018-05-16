f75eba5f7e8a9954e04738a2e402b0bf672b9c09
dibashi <dibashi1989@163.com>
2018-05-16 11:02:56 +0800

    添加测试数据

--- aircraftBattle/assets/Script/enemyPlaneDatas.js
+++ aircraftBattle/assets/Script/enemyPlaneDatas.js
@@ -28,7 +28,31 @@ var _stage = [
         {enemyID:0},{enemyID:4}
     ],
     [
-        {enemyID:2},{enemyID:3},{enemyID:1}
+        {enemyID:0},{enemyID:3},{enemyID:1}
+    ],
+    [
+        {enemyID:2},{enemyID:0},{enemyID:1}
+    ],
+    [
+        {enemyID:4},{enemyID:4},{enemyID:1}
+    ],
+    [
+        {enemyID:0},{enemyID:2},{enemyID:1}
+    ],
+    [
+        {enemyID:2},{enemyID:3},{enemyID:3}
+    ],
+    [
+        {enemyID:4},{enemyID:0},{enemyID:1}
+    ],
+    [
+        {enemyID:0},{enemyID:3},{enemyID:1}
+    ],
+    [
+        {enemyID:2},{enemyID:0},{enemyID:1}
+    ],
+    [
+        {enemyID:0},{enemyID:3},{enemyID:1}
     ]
 ];
 
