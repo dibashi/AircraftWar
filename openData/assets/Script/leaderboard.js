d693274855c86d2a52554bb66c872878a6c7e800
dibashi <dibashi1989@163.com>
2018-06-07 11:08:35 +0800

    实现主 子 域交互

--- openData/assets/Script/leaderboard.js
+++ openData/assets/Script/leaderboard.js
@@ -6,13 +6,13 @@ cc.Class({
             default: null,
             type: cc.Label
         },
-        // defaults, set visually when attaching this script to the Canvas
-        text: 'Hello, World!'
+        // // defaults, set visually when attaching this script to the Canvas
+        // text: 'Hello, World!'
     },
 
     // use this for initialization
     onLoad: function () {
-        this.label.string = this.text;
+       // this.label.string = this.text;
     },
 
     // called every frame
