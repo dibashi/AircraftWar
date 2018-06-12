4eb54a6cbadd4cecc4e8a0615d006dc1f7266e80
dibashi <dibashi1989@163.com>
2018-06-12 14:59:48 +0800

    数值修改

--- aircraftBattle/assets/NewScript/enemyPlaneDatas.js
+++ aircraftBattle/assets/NewScript/enemyPlaneDatas.js
@@ -46,11 +46,11 @@ var _wuDiTime = 8;
 //gid从1开始
 //bulletType 已失效！！！这个数据暂时先不做，无论如何设置都是根据当前敌机bullet0预制体来设置的！
 var _enemyPlaneData = [
-    {enemyID:0, planeImage: "enemyPlane0", blood: 5, shootingSpeed: 0.5, flyingSpeed: 4, bulletType: _bulletType.jipao, damage: 1,dropProbability:0.5,fallingObject:_generateType.jinbi,bulletTrack:_bulletTrack.sanfazhixian,enemyTrack:_enemyTrack.zuoyoushangxia },
-    {enemyID:1, planeImage: "enemyPlane1", blood: 5, shootingSpeed: 0.5, flyingSpeed: 3, bulletType: _bulletType.huopao, damage: 1,dropProbability:0.1,fallingObject:_generateType.wudichongci,bulletTrack:_bulletTrack.banquan,enemyTrack:_enemyTrack.zuoyoushangxia },
-    {enemyID:2, planeImage: "enemyPlane2", blood: 5, shootingSpeed: 0.5, flyingSpeed: 4, bulletType: _bulletType.jipao, damage: 1,dropProbability:0.5,fallingObject:_generateType.jinbi,bulletTrack:_bulletTrack.dingwei,enemyTrack:_enemyTrack.zuoyoushangxia },
-    {enemyID:3, planeImage: "enemyPlane3", blood: 5, shootingSpeed: 0.5, flyingSpeed: 4, bulletType: _bulletType.huopao, damage: 2,dropProbability:0.3,fallingObject:_generateType.jisushesu,bulletTrack:_bulletTrack.yiquan,enemyTrack:_enemyTrack.zuoyoushangxia },
-    {enemyID:4, planeImage: "enemyPlane4", blood: 5, shootingSpeed:0.5, flyingSpeed: 4, bulletType: _bulletType.huopao, damage: 2,dropProbability:0.1,fallingObject:_generateType.huojianpao,bulletTrack:_bulletTrack.wufasanshe,enemyTrack:_enemyTrack.zuoyoushangxia },
+    {enemyID:0, planeImage: "enemyPlane0", blood: 5, shootingSpeed: 0.5, flyingSpeed: 7, bulletType: _bulletType.jipao, damage: 1,dropProbability:0.5,fallingObject:_generateType.jinbi,bulletTrack:_bulletTrack.sanfazhixian,enemyTrack:_enemyTrack.zuoyoushangxia },
+    {enemyID:1, planeImage: "enemyPlane1", blood: 5, shootingSpeed: 0.5, flyingSpeed: 7, bulletType: _bulletType.huopao, damage: 1,dropProbability:0.1,fallingObject:_generateType.wudichongci,bulletTrack:_bulletTrack.banquan,enemyTrack:_enemyTrack.zuoyoushangxia },
+    {enemyID:2, planeImage: "enemyPlane2", blood: 5, shootingSpeed: 0.5, flyingSpeed: 7, bulletType: _bulletType.jipao, damage: 1,dropProbability:0.5,fallingObject:_generateType.jinbi,bulletTrack:_bulletTrack.dingwei,enemyTrack:_enemyTrack.zuoyoushangxia },
+    {enemyID:3, planeImage: "enemyPlane3", blood: 5, shootingSpeed: 0.5, flyingSpeed: 7, bulletType: _bulletType.huopao, damage: 2,dropProbability:0.3,fallingObject:_generateType.jisushesu,bulletTrack:_bulletTrack.yiquan,enemyTrack:_enemyTrack.zuoyoushangxia },
+    {enemyID:4, planeImage: "enemyPlane4", blood: 5, shootingSpeed:0.5, flyingSpeed: 7, bulletType: _bulletType.huopao, damage: 2,dropProbability:0.1,fallingObject:_generateType.huojianpao,bulletTrack:_bulletTrack.wufasanshe,enemyTrack:_enemyTrack.zuoyoushangxia },
 
 ];
 
@@ -81,16 +81,7 @@ var _stage = [
         {enemyID:2},{enemyID:3}
     ],
     [
-        {enemyID:4},
-    ],
-    [
         {enemyID:3},{enemyID:2},{enemyID:3}
-    ],
-    [
-        {enemyID:1},{enemyID:0}
-    ],
-    [
-        {enemyID:1},{enemyID:0}
     ],
     [
         {enemyID:0},{enemyID:4},{enemyID:1}
@@ -118,6 +109,9 @@ var _stage = [
     ],
     [
         {enemyID:0},{enemyID:1},{enemyID:2},{enemyID:2},{enemyID:4}
+    ]
+    [
+        {enemyID:2},{enemyID:2},{enemyID:2},{enemyID:2},{enemyID:2}
     ]
 ];
 
